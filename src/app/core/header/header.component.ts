e4e035ce8ad63aee2d2125f9493ddb9d8a12a92c
LisaMoulis <lisouille.moulis@gmail.com>
2021-09-06 17:09:24 +0200

    Added the login link

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Input()
+  login : string= "";
   constructor() { }
 
   ngOnInit(): void {
