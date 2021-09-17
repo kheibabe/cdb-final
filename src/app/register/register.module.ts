e509fdd821406d96f648a99e11f45f022783b7bf
LisaMoulis <lisouille.moulis@gmail.com>
2021-09-17 10:33:43 +0200

    Added register/connection proposition

--- src/app/register/register.module.ts
+++ src/app/register/register.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { CustomMaterialModule } from '../custom-material/custom-material.module';
 import { SharedModule } from '../shared/shared.module';
 import { RegisterComponent } from './register.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -14,6 +15,7 @@ import { RegisterComponent } from './register.component';
   ],
   imports: [
     CommonModule,
+    RouterModule,
     HttpClientModule,
     FormsModule,
     CustomMaterialModule,
